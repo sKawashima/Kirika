7c6d5f26b4ff4210d542a86b8fe89166edce07eb
sKawashima <bysk.333@gmail.com>
2020-03-20 15:26:19 +0900

    :pencil2: Make minimum

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,19 @@ import app from './initBolt'
 
 (async () => {
   await app.start(process.env.PORT || 3000)
-  console.log("⚡️ Bolt app is running")
+  console.log('⚡️ Bolt app is running')
 })()
+
+app.event("app_home_opened", async ({ event, say }) => {
+  say("ﾃﾞｰｽ!");
+});
+
+app.message("デス", async ({ message, say }) => {
+  say("デース！");
+});
+
+app.command("/say", async ({ command, ack, say }) => {
+  ack();
+
+  say(`${command.text}`);
+});
