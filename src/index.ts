b0efc1e5262f84c5519560cca390c13700e51a71
sKawashima <bysk.333@gmail.com>
2020-03-28 18:50:32 +0900

    Add channel_joined event

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ import app from './initBolt'
   console.log('⚡️ Bolt app is running')
 })()
 
+app.command("/say-kirika", async ({ command, ack, say }) => {
+  ack();
+
+  say(`${command.text}:desu:`);
+});
+
+
 app.event('app_home_opened', async ({ event, say }) => {
   say(':de-su:')
 })
 
+app.event("channel_joined", async ({ event, say }) => {
+  say("よろしく:de-su:！");
+});
+
 app.message('デス', async ({ message, say }) => {
   say(':de-su:')
-})
-
-app.command('/say-kirika', async ({ command, ack, say }) => {
-  ack()
-
-  say(`${command.text}:desu:`)
 })
