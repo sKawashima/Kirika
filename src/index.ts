35e7175a9cf73d7f774af4957b5baea28f1dc5ff
sKawashima <bysk.333@gmail.com>
2020-03-28 18:46:46 +0900

    Add Prettier, node-modern, lint-staged

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import app from './initBolt'
-
-(async () => {
+;(async () => {
   await app.start(process.env.PORT || 3000)
   console.log('⚡️ Bolt app is running')
 })()
 
-app.event("app_home_opened", async ({ event, say }) => {
-  say(":de-su:")
+app.event('app_home_opened', async ({ event, say }) => {
+  say(':de-su:')
 })
 
-app.message("デス", async ({ message, say }) => {
-  say(":de-su:")
+app.message('デス', async ({ message, say }) => {
+  say(':de-su:')
 })
 
-app.command("/say-kirika", async ({ command, ack, say }) => {
+app.command('/say-kirika', async ({ command, ack, say }) => {
   ack()
 
   say(`${command.text}:desu:`)
