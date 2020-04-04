ca6ca26eed1f9c13157fc33db612a6d3feb9c191
Shota Kawashima <bysk.333@gmail.com>
2020-04-04 16:05:18 +0900

    Remove members text (#13)

--- src/functions.ts
+++ src/functions.ts
@@ -15,7 +15,7 @@ export const getChannelsList = async ({ token }) => {
     let channelsListText = ''
     getChannelsListResponce.channels.forEach(channel => {
       if (!channel.is_archived)
-        channelsListText += `<#${channel.id}|${channel.name}> ${channel.topic.value} (${channel.members.length} members)\n`
+        channelsListText += `<#${channel.id}|${channel.name}> ${channel.topic.value}\n`
     })
 
     return `チャンネル一覧 :de-su:\n\n${channelsListText}`
