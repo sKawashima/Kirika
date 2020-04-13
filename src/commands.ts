b8e2ab9d4cecc32107b049683edd96e4192ae1c5
Shota Kawashima <bysk.333@gmail.com>
2020-04-13 19:15:17 +0900

    Add kirika-mahjong command (#19)

--- src/commands.ts
+++ src/commands.ts
@@ -4,19 +4,33 @@ import { getChannelsList } from './functions'
 const initCommands = () => {
   app.command('/kirika-say', async ({ command, ack, say }) => {
     ack()
-    say(`${command.text}:desu:`)
+    say(`${command.text}:de-su:`)
   })
 
-  app.command(
-    '/kirika-channel-list',
-    async ({ command, ack, say, context }) => {
-      ack()
-      const channelsListText = await getChannelsList({
-        token: context.botToken
+  app.command('/kirika-channel-list', async ({ ack, say, context }) => {
+    ack()
+    const channelsListText = await getChannelsList({
+      token: context.botToken
+    })
+    say(channelsListText)
+  })
+
+  app.command('/kirika-mahjong', async ({ command, ack, context, say }) => {
+    ack()
+
+    try {
+      const postMessageResponce = await app.client.chat.postMessage({
+        token: context.botToken,
+        channel: '帝国麻雀部',
+        text: `@here ${command.text}:de-su:\nhttps://hama-empire.slack.com/files/UECKGJR0B/F011XMYCK6C/pic-mj1.png`,
+        link_names: true
       })
-      say(channelsListText)
+    } catch (err) {
+      console.log(err)
     }
-  )
+
+    say(':desu:')
+  })
 }
 
 export default initCommands
