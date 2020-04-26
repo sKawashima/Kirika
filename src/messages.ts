175906ba8899f6bf27d7ab92e3d50ab2e051195d
sKawashima <bysk.333@gmail.com>
2020-04-26 23:42:47 +0900

    Make lgtm interaction

--- src/messages.ts
+++ src/messages.ts
@@ -1,8 +1,14 @@
 import app from './initBolt'
+import { lgtmList } from './imageList'
 
 const initMessages = () => {
-  app.message('デス', async ({ message, say }) => {
+  app.message('デス', async ({ say }) => {
     say(':de-su:')
+  })
+
+  app.message(/^(LGTM|lgtm)/, async ({ say }) => {
+    const lgtmImageURL = lgtmList[Math.floor(Math.random() * lgtmList.length)]
+    say(lgtmImageURL)
   })
 }
 
