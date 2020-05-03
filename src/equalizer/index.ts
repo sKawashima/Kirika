ecedbec67a4eba47f47786985b4304fa84ec745d
sKawashima <bysk.333@gmail.com>
2020-05-03 18:40:49 +0900

    Make reimbursement price logic

--- src/equalizer/index.ts
+++ src/equalizer/index.ts
@@ -1,7 +1,11 @@
 import setupData from './setupData'
+import dataToBurden from './dataToBurden'
 
 const makeReimbursementData = async () => {
   const data = await setupData()
+  const reimbursementPrice =
+    (dataToBurden(data.sKData) - dataToBurden(data.ryuData)) / 2 +
+    dataToBurden(data.sKFixedData)
 }
 
 makeReimbursementData()
