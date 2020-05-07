0f1a46a49bb8f452da18dd20390aef387019eb96
Shota Kawashima <bysk.333@gmail.com>
2020-05-07 22:24:42 +0900

    Add username to commands (#27)
    * Add username to commands
    
    * Fix props

--- src/commands.ts
+++ src/commands.ts
@@ -3,12 +3,12 @@ import { getChannelsList } from './functions'
 import equalizer from './equalizer'
 
 const initCommands = () => {
-  app.command('/kirika-say', async ({ command, ack, say }) => {
+  app.command('/kirika-say', async ({ ack, command, say }) => {
     ack()
     say(`${command.text}:de-su:`)
   })
 
-  app.command('/kirika-channel-list', async ({ ack, say, context }) => {
+  app.command('/kirika-channel-list', async ({ ack, context, say }) => {
     ack()
     const channelsListText = await getChannelsList({
       token: context.botToken
@@ -16,15 +16,16 @@ const initCommands = () => {
     say(channelsListText)
   })
 
-  app.command('/kirika-mahjong', async ({ command, ack, context, say }) => {
+  app.command('/kirika-mahjong', async ({ ack, command, context }) => {
     ack()
+    const userName = command.user_name
     try {
       const postMessageResponce = await app.client.chat.postMessage({
         token: context.botToken,
         channel: '帝国麻雀部',
         text: `@here ${
           command.text !== '' ? `${command.text}:de-su:` : ''
-        }\nhttps://hama-empire.slack.com/files/UECKGJR0B/F011XMYCK6C/pic-mj1.png`,
+        }\n\n*from:* @${userName}\nhttps://hama-empire.slack.com/files/UECKGJR0B/F011XMYCK6C/pic-mj1.png`,
         link_names: true
       })
     } catch (err) {
@@ -32,10 +33,11 @@ const initCommands = () => {
     }
   })
 
-  app.command('/equalizer', async ({ ack, say }) => {
+  app.command('/equalizer', async ({ ack, command, say }) => {
     ack()
+    const userName = command.user_name
     const message = await equalizer()
-    say(message)
+    say(`${message}\n\n*from:* @${userName}`)
   })
 }
 
