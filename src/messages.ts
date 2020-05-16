2f7b9fbd1b5d0309d7c19e950fec4735f9beabc6
sKawashima <bysk.333@gmail.com>
2020-05-16 18:06:04 +0900

    Add meu

--- src/messages.ts
+++ src/messages.ts
@@ -2,9 +2,28 @@ import app from './initBolt'
 import { lgtmList } from './imageList'
 
 const initMessages = () => {
-  app.message('デス', async ({ say }) => {
-    say(':de-su:')
-  })
+  app.message(
+    /(です|でーす|デス|デース|desu|de-su|:dededede-su:)/,
+    ({ say }) => {
+      const messages = [
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':de-su:',
+        ':desu:',
+        ':desu::desu::de-su:',
+        ':dededede-su:',
+        'https://lohas.nicoseiga.jp/thumb/5661682i',
+        'https://pbs.twimg.com/media/Dal0dcJV4AEaY0p.jpg'
+      ]
+      say(messages[Math.floor(Math.random() * messages.length)])
+    }
+  )
 
   app.message(/(LGTM|lgtm)/, async ({ say }) => {
     const lgtmImageURL = lgtmList[Math.floor(Math.random() * lgtmList.length)]
