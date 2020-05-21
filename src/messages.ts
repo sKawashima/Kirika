f0dfd2e59bf1ec4891ba60b0ff08ebe45d079962
sKawashima <bysk.333@gmail.com>
2020-05-21 21:56:07 +0900

    remove です

--- src/messages.ts
+++ src/messages.ts
@@ -2,28 +2,25 @@ import app from './initBolt'
 import { lgtmList } from './imageList'
 
 const initMessages = () => {
-  app.message(
-    /(です|でーす|デス|デース|desu|de-su|:dededede-su:)/,
-    ({ say }) => {
-      const messages = [
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':de-su:',
-        ':desu:',
-        ':desu::desu::de-su:',
-        ':dededede-su:',
-        'https://lohas.nicoseiga.jp/thumb/5661682i',
-        'https://pbs.twimg.com/media/Dal0dcJV4AEaY0p.jpg'
-      ]
-      say(messages[Math.floor(Math.random() * messages.length)])
-    }
-  )
+  app.message(/(でーす|デス|デース|desu|de-su|:dededede-su:)/, ({ say }) => {
+    const messages = [
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':de-su:',
+      ':desu:',
+      ':desu::desu::de-su:',
+      ':dededede-su:',
+      'https://lohas.nicoseiga.jp/thumb/5661682i',
+      'https://pbs.twimg.com/media/Dal0dcJV4AEaY0p.jpg'
+    ]
+    say(messages[Math.floor(Math.random() * messages.length)])
+  })
 
   app.message(/(LGTM|lgtm)/, async ({ say }) => {
     const lgtmImageURL = lgtmList[Math.floor(Math.random() * lgtmList.length)]
