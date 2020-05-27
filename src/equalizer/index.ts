806184f399dc166239ebe6668ed2c855f685c289
Shota Kawashima <bysk.333@gmail.com>
2020-05-27 18:31:11 +0900

    Revert "test (#36)"
    This reverts commit fa764d6c917845879c4bdc49be5fb65ec6bde62e.

--- src/equalizer/index.ts
+++ src/equalizer/index.ts
@@ -20,13 +20,13 @@ ryu_g → sK 固定費
 ${dataToTextTable(data.sKFixedData)}
 合計: ${dataToBurden(data.sKFixedData)} 円
 
-ryu_g負担分
-${dataToTextTable(data.ryuData)}
-合計: ${dataToBurden(data.ryuData)} 円
-
 sK負担分
 ${dataToTextTable(data.sKData)}
 合計: ${dataToBurden(data.sKData)} 円
+
+ryu_g負担分
+${dataToTextTable(data.ryuData)}
+合計: ${dataToBurden(data.ryuData)} 円
 
 ryu_g → sK 精算額
 ${(dataToBurden(data.sKData) - dataToBurden(data.ryuData)) / 2 +
