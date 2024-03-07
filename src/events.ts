b828bcf00cfe2638cd88c2ec3b4062c8621cf4a7
Kawashima Shota (shiba) <bysk.333@gmail.com>
2024-03-07 21:43:44 +0900

    Remove talk (#92)
    * 🔥 refactor(events.ts): remove unused import of getTalkResponce function
    🔥 refactor(talk): remove unused talk module and its related test file
    
    * 📝 docs(README.md): add setup instructions for Kirila Slack BOT including environment variables and commands for installation and running the bot

--- src/events.ts
+++ src/events.ts
@@ -1,5 +1,4 @@
 import app from './initBolt'
-import { getTalkResponce } from './talk'
 
 const initEvents = () => {
   app.event('channel_created', async ({ event, context }) => {
@@ -13,18 +12,6 @@ const initEvents = () => {
     } catch (err) {
       console.log(err)
     }
-  })
-
-  app.event('app_mention', async ({ say, event, context }) => {
-    const reply = await getTalkResponce(
-      event.text.replace(`<@${context.botUserId}>`, '')
-    )
-    await say(
-      `${reply.replace(/(です|ですよ|ですよね)$/u, '')}:de-su:`.replace(
-        /(ですか?:de-su:)$/u,
-        ':desu:か?:'
-      )
-    )
   })
 }
 
