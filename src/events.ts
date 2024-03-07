95af760f8bac2ffd013aee510dbc062cccb2c778
sKawashima <bysk.333@gmail.com>
2024-03-07 22:37:04 +0900

    🔧 refactor(events.ts): extract getUrl function to improve code readability and reusability 🚀 feat(events.ts): add functionality to extract and handle URLs in app_mention events

--- src/events.ts
+++ src/events.ts
@@ -1,5 +1,11 @@
 import app from './initBolt'
 
+const getUrl = (text: string) => {
+  const urlPattern = /<(https?:\/\/[^\s]+)>/g
+  const urls = text.match(urlPattern) || []
+  return urls
+}
+
 const initEvents = () => {
   app.event('channel_created', async ({ event, context }) => {
     try {
@@ -11,6 +17,42 @@ const initEvents = () => {
       })
     } catch (err) {
       console.log(err)
+    }
+  })
+
+  app.event('app_mention', async ({ say, event, context, client }) => {
+    const thread_ts = event.thread_ts || event.ts
+
+    const urls = getUrl(event.text)
+
+    if (urls.length > 0) {
+      say({
+        text: `見つかったURL: ${urls.join(', ')}`,
+        thread_ts
+      })
+    } else {
+      const replies = await client.conversations.replies({
+        token: context.botToken,
+        channel: event.channel,
+        ts: thread_ts
+      })
+
+      const urlInReplies = replies.messages
+        .map(m => getUrl(m.text))
+        .flat()
+        .filter(u => u)
+
+      if (!replies.messages || replies.messages.length === 1) {
+        return
+      }
+
+      console.log(replies.messages.map(m => m.text).join('\n---\n'))
+      console.log(urlInReplies)
+
+      say({
+        text: 'URLは見つかりませんでした',
+        thread_ts
+      })
     }
   })
 }
