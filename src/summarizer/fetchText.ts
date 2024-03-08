f63d1f9c5e525a65d0c9c7525d5a9e1e20dcc1f7
sKawashima <bysk.333@gmail.com>
2024-03-08 13:18:27 +0900

    📝 (fetchText.ts): refactor fetchText function to improve readability and maintainability by extracting HTML parsing logic into a separate function parseHTMLToMarkdown

--- src/summarizer/fetchText.ts
+++ src/summarizer/fetchText.ts
@@ -2,20 +2,46 @@ import axios from 'axios'
 import { JSDOM } from 'jsdom'
 import TurndownService from 'turndown'
 
+const turndownService = new TurndownService()
+turndownService.addRule('ignoreScriptStyleSvg', {
+  filter: function (node) {
+    return (
+      node.nodeName === 'SCRIPT' ||
+      node.nodeName === 'STYLE' ||
+      node.nodeName === 'SVG' ||
+      node.nodeName === 'IMG'
+    )
+  },
+  replacement: function () {
+    return '' // 空文字を返して、これらの要素をマークダウン出力から除去
+  }
+})
+
 export const fetchText = async (url: string) => {
   try {
     const { data: html } = await axios.get(url)
-    // JSDOMでHTMLを解析
-    const dom = new JSDOM(html)
-    const { document } = dom.window
-    const bodyHtml = document.querySelector('body').innerHTML
-
-    const turndownService = new TurndownService()
-    const markdown = turndownService.turndown(bodyHtml)
+    const markdown = parseHTMLToMarkdown(html)
 
     return markdown
   } catch (error) {
     console.error('エラーが発生しました:', error)
     return null
   }
+}
+
+const parseHTMLToMarkdown = (html: string) => {
+  const dom = new JSDOM(html)
+  const { document } = dom.window
+
+  const styles = document.querySelectorAll('style')
+  styles.forEach(style => style.remove())
+
+  const scripts = document.querySelectorAll('script')
+  scripts.forEach(script => script.remove())
+
+  const bodyHtml = document.querySelector('body').innerHTML
+
+  const markdown = turndownService.turndown(bodyHtml)
+
+  return markdown
 }
